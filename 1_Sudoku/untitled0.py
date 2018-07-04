670754b6115a1d9004c5de9433a84394dde463eb
Shunling Guo <shunling@Shunlings-MacBook-Pro.local>
2018-07-03 23:09:10 -0700

    First commit

---
+++ 1_Sudoku/untitled0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 23 23:08:48 2018
+
+@author: shunling
+"""
+import pygame
+from pygame.locals import*
+img = pygame.image.load('lena_gray.bmp')
+
+print("Yes")
